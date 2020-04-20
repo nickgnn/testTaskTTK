681f5e9ce9949cc43da29bc7162c39e9afa36598
nick <gnn@mm.st>
2020-04-20 21:50:05 +0300

    add starter

--- src/main/java/my/test/task/AppSpaceX.java
+++ src/main/java/my/test/task/AppSpaceX.java
@@ -1,4 +1,11 @@
 package my.test.task;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AppSpaceX {
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpaceX.class, args);
+    }
 }
